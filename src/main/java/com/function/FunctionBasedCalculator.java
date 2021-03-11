d3a89a27c8d1ffb03e39160b6786119f149f6360
Alexandra Simonova <mazukata@gmail.com>
2021-03-12 02:10:56 +0300

    add function based calculator

---
+++ src/main/java/com/function/FunctionBasedCalculator.java
@@ -0,0 +1,27 @@
+package com.function;
+
+import java.util.Map;
+import java.util.function.BinaryOperator;
+
+public class FunctionBasedCalculator {
+    Map<OperationType, BinaryOperator<Integer>> map = Map.of(OperationType.ADDITION, (x, y) -> x + y,
+            OperationType.SUBTRACTION, (x, y) -> x - y,
+            OperationType.MULTIPLICATION, (x, y) -> x * y,
+            OperationType.DIVISION, (x, y) -> x / y);
+
+    private int sum(int a, int b) {
+        return a + b;
+    }
+
+    private int subtract(int a, int b) {
+        return a - b;
+    }
+
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+
+    private double divide(int a, int b) {
+        return (double) a / b;
+    }
+}
